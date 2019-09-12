f84ab483c6c3aea3c83d4da0afb14baf51e4aa13
Dimas Kamurapi <dkamurapi@gmail.com>
2019-09-12 10:00:22 +0700

    update

--- Pertemuan 2/NotepadApp/app/src/main/java/com/kamurapi/notepadapp/MainActivity.java
+++ Pertemuan 2/NotepadApp/app/src/main/java/com/kamurapi/notepadapp/MainActivity.java
@@ -1,13 +1,72 @@
 package com.kamurapi.notepadapp;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+
+import com.kamurapi.notepadapp.adapters.NoteAdapter;
+import com.kamurapi.notepadapp.model.Note;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArrayList<Note> notes;
+    private FloatingActionButton fab;
+    private NoteAdapter adapter;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //init recycler
+        recyclerView = findViewById(R.id.note_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        //init flying button
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAddNewNote();
+            }
+        });
+    }
+
+    private void loadNotes() {
+        this.notes = new ArrayList<>();
+        for (int i = 1; i<12; i++){
+            notes.add(new Note("ye let's write your note",new Date().getTime()));
+
+            adapter = new NoteAdapter(this,notes);
+            recyclerView.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+
+        }
+    }
+
+    private void onAddNewNote() {
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadNotes();
     }
 }
--- Pertemuan 2/NotepadApp/app/src/main/java/com/kamurapi/notepadapp/adapters/NoteAdapter.java
+++ Pertemuan 2/NotepadApp/app/src/main/java/com/kamurapi/notepadapp/adapters/NoteAdapter.java
@@ -1,29 +1,61 @@
 package com.kamurapi.notepadapp.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.kamurapi.notepadapp.R;
+import com.kamurapi.notepadapp.model.Note;
+import com.kamurapi.notepadapp.utils.NoteUtils;
+
+
+import java.util.ArrayList;
+
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder>{
+
+    private Context context;
+    private ArrayList<Note> notes;
+
+    public NoteAdapter(Context context, ArrayList<Note> notes) {
+        this.context = context;
+        this.notes = notes;
+
+    }
+
+
     @NonNull
     @Override
     public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_layout, parent, false);
+        return new NoteHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
-
+        Note note = getNote(position);
+        if (note!=null){
+            holder.noteText.setText(note.getNoteText());
+            holder.noteDate.setText(NoteUtils.dateFromLong(note.getNoteDate()));
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return notes.size();
     }
 
+    private Note getNote(int position) {
+        return notes.get(position);
+    }
     class NoteHolder extends RecyclerView.ViewHolder{
         TextView noteText, noteDate;
+        public NoteHolder(View itemView){
+            super(itemView);
+        }
     }
 }
 
